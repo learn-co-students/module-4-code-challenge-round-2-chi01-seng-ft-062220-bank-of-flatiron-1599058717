905de26c5c792fc7a60d48905f1ab453057b0b1c
Simon51584 <simon51584@gmail.com>
2020-09-02 12:09:15 -0500

    seth's update

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,15 +4,47 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+
+  constructor() {
+    super()	  
+    this.state = {
+      store: [],
+      transactions: [],
+      searching: ''}
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:6001/transactions")
+    .then(resp => resp.json())
+    .then(results => this.setState({
+      store: results,
+      transactions: results
+    }))
+  }	 
+  
+  handleChange = (e) => {
+    const { store, searching } = this.state
+    this.setEachQuery(e)
+
+    const searchResults = store.filter(transaction => transaction.category.toLowerCase().includes(searching) || transaction.description.toLowerCase().includes(searching))
+    this.setState({transactions: searchResults})
+  }	  
+
+  setEachQuery = (e) => {
+    this.setState({searching: e.target.value.toLowerCase()})
+  }
+  
   render() {
     return (
       <div>
-        <Search />
+        <Search handleChange={this.handleChange}/>
         <AddTransactionForm />
-        <TransactionsList />
+        <TransactionsList transactions={this.state.transactions}/>
       </div>
     );
   }
 }
+
+
 
 export default AccountContainer;
