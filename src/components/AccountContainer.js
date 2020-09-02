cd8edf487d16a1ce9001495b77ae178127dbc04d
msguttman <msguttman@gmail.com>
2020-09-02 11:27:25 -0500

    changes

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,12 +4,14 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+
+
   render() {
     return (
       <div>
         <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <AddTransactionForm newTrans={this.props.newTrans} />
+        <TransactionsList transactions={this.props.transactions} />
       </div>
     );
   }
--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,69 @@ import React, { Component } from "react";
 import AccountContainer from "./AccountContainer";
 import "../stylesheets/App.css";
 
+
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      allTrans: [],
+      searchTerm: ""
+    }
+  }
+
+
+
+  handleNewTrans = (e) => {
+    e.preventDefault()
+
+    const newTrans = {
+      date: e.target["date"].value,
+      description: e.target["description"].value,
+      category: e.target["category"].value,
+      amount: e.target["amount"].value
+    }
+
+    this.setState({
+      allTrans: [...this.state.allTrans, newTrans]
+    })
+
+    const reqObj = {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(newTrans)
+    }
+
+    fetch("http://localhost:6001/transactions", reqObj)
+  }
+
+
+  handleSearch = () => {
+
+    console.log(this.state.searchTerm)
+  }
+
+
+  componentDidMount() {
+    fetch("http://localhost:6001/transactions")
+    .then(resp => resp.json())
+    .then(transactions => {
+      this.setState({
+        allTrans: transactions
+      })
+    })
+  }
+
   render() {
     return (
       <div className="ui raised segment">
         <div className="ui segment violet inverted">
           <h2>The Royal Bank of Flatiron</h2>
         </div>
-        <AccountContainer />
+        <AccountContainer transactions={this.state.allTrans} newTrans={this.handleNewTrans} />
       </div>
     );
   }
