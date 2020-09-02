0793fa8c416994386c04f066c97220189568fae6
Sam M <sammontalvojr@gmail.com>
2020-09-02 11:17:29 -0500

    core deliverables completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,53 @@
 import React, { Component } from "react";
 import AccountContainer from "./AccountContainer";
 import "../stylesheets/App.css";
+const API = "http://localhost:6001/transactions";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      transactions: [],
+    };
+  }
+
+  getTransactions = async () => {
+    const res = await fetch(API);
+    const transactions = await res.json();
+    this.setState({
+      transactions: [...transactions],
+    });
+  };
+
+  postTransaction = async (newTransaction) => {
+    const reqObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTransaction),
+    };
+    const res = await fetch(API, reqObj);
+    const postedTransaction = await res.json();
+    this.setState({
+      transactions: [...this.state.transactions, postedTransaction],
+    });
+  };
+
+  componentDidMount() {
+    this.getTransactions();
+  }
+
   render() {
     return (
       <div className="ui raised segment">
         <div className="ui segment violet inverted">
           <h2>The Royal Bank of Flatiron</h2>
         </div>
-        <AccountContainer />
+        <AccountContainer
+          transactions={this.state.transactions}
+          postTransaction={this.postTransaction}
+        />
       </div>
     );
   }
